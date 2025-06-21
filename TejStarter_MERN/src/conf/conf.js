853b5c01d1ce392cdfb8e34807d1594931d32141
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-06-21 19:14:39 +0530

    feat: implement AuthService and Service classes for user authentication and data management

---
+++ TejStarter_MERN/src/conf/conf.js
@@ -0,0 +1,12 @@
+
+const conf = {  // an Object is crated to acees those variables
+
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+}
+
+
+export default conf 
+// export to make avalable in other files
