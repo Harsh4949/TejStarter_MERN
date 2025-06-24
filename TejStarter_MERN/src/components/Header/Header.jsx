d99d0697ac4165e3c8c5fce796064785da09e590
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-06-25 01:01:22 +0530

    feat: Update Header component navigation items and enhance styling

--- TejStarter_MERN/src/components/Header/Header.jsx
+++ TejStarter_MERN/src/components/Header/Header.jsx
@@ -22,23 +22,22 @@ function Header() {
           },
           {
               name: "Collaboration",
-              slug: "/all-posts",
+              slug: "/Collaboration",
           },
           {
               name: "Stories",
               slug: "/Stories",
           },
     ] 
-
-
+    
   return (
-    <header className='py-3 shadow bg-gray-500'>
+    <header className='py-3 shadow bg-gray-500 max-w-fit'>
       <Container>
         <nav className='flex'>
             <div className='mr-4'>
               <Link to='/'>
                 <Logo width='70px'   />
-
+                <h1> TejStarter</h1>
                 </Link>
             </div>
           <ul className='flex ml-auto'>
